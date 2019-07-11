a752a0662899b43dfc10a8af793c183e9e79a3c6
synchronous-unit-4097 <kamilparks.64@outlook.com>
2019-07-11 03:59:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,44 +3,30 @@ def create_an_empty_array
 end
 
 def create_an_array
-<<<<<<< HEAD
 [a,b,c,d]
 end
 
 def add_element_to_end_of_array(array, element)
   [a]<<e
-=======
-  []
-  {}
-  ()
-  <>
 end
 
 def add_element_to_end_of_array(array, element)
-  (#push)
->>>>>>> 5c999056ff7cb8be5b1f9b48268622973bd1b316
 end
 
 def add_element_to_start_of_array(array, element)
-  (#unshift)
 end
 
 def remove_element_from_end_of_array(array)
-  (#pop)
 end
 
 def remove_element_from_start_of_array(array)
-  (#shift)
 end
 
 def retrieve_element_from_index(array, index_number)
-  []
 end
 
 def retrieve_first_element_from_array(array)
-  []
 end
 
 def retrieve_last_element_from_array(array)
-  []
 end
