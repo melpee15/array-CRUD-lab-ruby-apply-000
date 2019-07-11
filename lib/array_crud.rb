7e900eef08d801acf565d6b447e7ad3ae8c4d1ec
synchronous-unit-4097 <kamilparks.64@outlook.com>
2019-07-11 03:52:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,22 @@ def create_an_empty_array
 end
 
 def create_an_array
+<<<<<<< HEAD
 [a,b,c,d]
 end
 
 def add_element_to_end_of_array(array, element)
   [a]<<e
+=======
+  []
+  {}
+  ()
+  <>
+end
+
+def add_element_to_end_of_array(array, element)
+  (#push)
+>>>>>>> 5c999056ff7cb8be5b1f9b48268622973bd1b316
 end
 
 def add_element_to_start_of_array(array, element)
